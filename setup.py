2b2470f0f21592f1863f482f01a5a1e6b1aaf480
Alain Péteut <alain.peteut@yahoo.com>
2014-07-02 18:26:13 +0200

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
@@ -10,13 +10,28 @@ if sys.version_info < required_version:
     raise SystemExit('circbuf requires Python {} or later'.format(
         '.'.join(map(str, required_version))))
 
+CLASSIFIERS = (
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python 3',
+    'Programming Language :: Python 3.3',
+    'Programming Language :: Python 3.4',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+)
+
+INSTALL_REQUIRES = ('',)
+
 setup(
     name='circbuf',
     version='0.0.0',
-    description='Circular Buffer for Python',
+    description='circular buffer for Python',
     long_description=README,
     author='Alain Péteut',
-    packages=('circbuf',),
+    author_email='alain.peteut@yahoo.com',
+    packages=find_packages(),
+    setup_requires=('nose>=1.0',),
     test_suite='nose.collector',
-    platforms='all'
+    platforms='all',
+    classifiers=CLASSIFIERS
 )
